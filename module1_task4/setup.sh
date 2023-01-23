9ba6910803aaebed9cbe67090dc21c54f770bfd2
Ghassen-bgh <3591@holbertonstudents.com>
2023-01-23 14:54:52 +0100

    setup fix

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -2,12 +2,12 @@
 
 ## Install Hugo && make
 
-apt-get update && apt-get install -y hugo make
+sudo apt-get update && apt-get install -y hugo make
 
 ## Using Curl to download hugo.deb
 
-curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
-apt-get install ./hugo.deb
+sudo curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
+sudo apt-get install ./hugo.deb
 
 ## build the site 
-make build
+sudo make build
